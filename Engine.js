bdc3d1504565d5be9e7713867b6304492ef438e1
iki1uc <entertrainer@web.de>
2026-08-14 02:52:12 +0200

    Update Engine.js

--- Engine.js
+++ Engine.js
@@ -1,10 +1,13 @@
 import { karga } from "../KARGA/KARGA.js";
-import { larBoost, larSuggest, larCompetence } from "./LAR3E.js";
+import { larBoost } from "./BOOST.js";
+import { larSuggest } from "./SUGGEST.js";
+import { larCompetence } from "./COMP.js";
 
-export function lar(name) {
+export function larEconomy(name) {
   const base = karga(name);
 
   return {
+    mode: "ECONOMY",
     modul: name,
     axis: base.result.axis,
     number: base.result.num,
